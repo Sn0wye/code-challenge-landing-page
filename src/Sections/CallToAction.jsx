a636d25b5963a5b664ae95e392764a976e867079
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-21 18:28:36 -0300

    final adjusts

--- src/Sections/CallToAction.jsx
+++ src/Sections/CallToAction.jsx
@@ -4,9 +4,9 @@ import calltoaction from '../Assets/img/calltoaction.svg'
 
 export default function CallToAction() {
   return (
-    <section className="flex w-full items-center justify-center">
-      <main className="flex w-3/4 rounded-3xl bg-purple-500 p-24 text-white">
-        <div className>
+    <section className="mt-24 flex w-full items-center justify-center">
+      <main className="max-w-1/2 flex w-3/4 rounded-3xl bg-purple-500 p-12 text-white">
+        <div className="p-12">
           <h1 className="text-5xl">Call to action section</h1>
           <p className="mt-6">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
@@ -17,7 +17,7 @@ export default function CallToAction() {
             Know more
           </button>
         </div>
-        <aside className="ml-64 flex  w-full items-center justify-center">
+        <aside className="flex  w-full items-center justify-center">
           <img
             src={calltoaction}
             alt="Girl and dog looking to a sword in a rock"
